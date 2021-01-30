28f213c32cb1fe1aeebb034e8cdd7f1e9ce0f9ed
Ahmad M.Sayed <ahmed.sayed.fcis1997@gmail.com>
2021-01-30 23:54:38 +0200

    error handler middlewware

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,25 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
+import prductRouter from './routes/productRouter.js'
 dotenv.config()
 
 const app = express()
 connectDB()
 app.use(morgan('tiny'))
 app.use(cors())
+
 app.get('/', (req, res) => {
   return res.status(200).send('API work')
 })
 
-app.get('/api/products', (req, res) => {
-  return res.status(200).json(products)
-})
+app.use('/api/products', prductRouter)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(
